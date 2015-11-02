b246b5fa5783fee000dbffe144db301bb49ae9d8
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-11-02 17:29:19 +1100

    test path

--- src/stages.py
+++ src/stages.py
@@ -67,7 +67,7 @@ class Stages(object):
     def align_bwa(self, inputs, bam_out, sample_id):
         '''Align the paired end fastq files to the reference genome using bwa'''
         fastq_read1_in, fastq_read2_in = inputs
-        print "Here working with " + str(inputs)
+        print "Here working with " + str(fastq_read1_in)
         cores = self.get_stage_options('align_bwa', 'cores')
         safe_make_dir('"alignments/{sample}"'.format(sample=sample_id))
         read_group = '"@RG\tID:{readid}\tSM:{sample}\tPL:Illumina"' \
