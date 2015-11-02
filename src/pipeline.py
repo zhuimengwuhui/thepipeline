ad9d8115c2f5e4228c3f6cd81887fb62c6411ed0
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-11-02 16:17:01 +1100

    testing RG data

--- src/pipeline.py
+++ src/pipeline.py
@@ -42,7 +42,10 @@ def make_pipeline(state):
         # Add an "extra" argument to the state (beyond the inputs and outputs)
         # which is the sample name. This is needed within the stage for finding out
         # sample specific configuration options
-        extras=['{readid[0]}' '{lib[0]}' '{lane[0]}' '{sample[0]}'],
+        extras=['{readid[0]}'],
+        extras=['{lib[0]}'],
+        extras=['{lane[0]}'],
+        extras=['{sample[0]}'],
         # The output file name is the sample name with a .bam extension.
         output='{path[0]}/alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.bam')
 
