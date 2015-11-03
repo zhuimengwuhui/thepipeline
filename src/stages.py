d2fd95ea41ece3679d8dd453ce0296867ae657f0
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-11-04 10:51:55 +1100

    test RG

--- src/stages.py
+++ src/stages.py
@@ -72,8 +72,8 @@ class Stages(object):
         # safe_make_dir('alignments/{sample}'.format(sample=sample_id))
         # read_group = '"@RG\\tID:{readid}\\tPU:{lib}\\tLN:{lane}\\tSM:{sample}\\tPL:Illumina"' \
         read_group = '"@RG\\tID:{sample}\\tSM:{sample}\\tPL:Illumina"' \
-            # .format(readid=read_id, lib=lib, lane=lane, sample=sample_id)
             .format(sample=sample_id)
+            # .format(readid=read_id, lib=lib, lane=lane, sample=sample_id)
         command = 'bwa mem -t {cores} -R {read_group} {reference} {fastq_read1} {fastq_read2} ' \
                   '| samtools view -b -h -o {bam} -' \
                   .format(cores=cores,
