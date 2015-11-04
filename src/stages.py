9f554c6eaf92fa25dcf38516de702bb48b25f43a
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-11-04 16:53:24 +1100

    new RG lib

--- src/stages.py
+++ src/stages.py
@@ -70,10 +70,8 @@ class Stages(object):
         fastq_read1_in, fastq_read2_in = inputs
         cores = self.get_stage_options('align_bwa', 'cores')
         # safe_make_dir('alignments/{sample}'.format(sample=sample_id))
-        # read_group = '"@RG\\tID:{readid}\\tPU:{lib}\\tLN:{lane}\\tSM:{sample}\\tPL:Illumina"' \
-        read_group = '"@RG\\tID:{sample}\\tSM:{sample}\\tPL:Illumina"' \
-            .format(sample=sample_id)
-            # .format(readid=read_id, lib=lib, lane=lane, sample=sample_id)
+        read_group = '"@RG\\tID:{readid}\\tSM:{sample}\\tPU:lib1\\tLN:{lane}\\tPL:Illumina"' \
+            .format(readid=read_id, lib=lib, lane=lane, sample=sample_id)
         command = 'bwa mem -t {cores} -R {read_group} {reference} {fastq_read1} {fastq_read2} ' \
                   '| samtools view -b -h -o {bam} -' \
                   .format(cores=cores,
