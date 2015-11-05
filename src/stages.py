f340db021e132415f101708e2a10f0a9812b346a
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-11-05 11:05:46 +1100

    fix path to picard

--- src/stages.py
+++ src/stages.py
@@ -11,7 +11,7 @@ from utils import safe_make_dir
 from runner import run_stage
 import os
 
-PICARD_JAR = '$PICARD_HOME/lib/picard.jar'
+PICARD_JAR = '$PICARD_HOME/picard.jar'
 GATK_JAR = '$GATK_HOME/GenomeAnalysisTK.jar'
 
 def java_command(jar_path, mem_in_gb, command_args):
