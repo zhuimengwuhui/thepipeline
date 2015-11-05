47f572c2e38e039f539e7ec3354b0f255cf4d048
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-11-05 17:48:10 +1100

    missing recall

--- src/pipeline.py
+++ src/pipeline.py
@@ -100,11 +100,12 @@ def make_pipeline(state):
         input=output_from('base_recalibration_gatk'),
         # filter=formatter('.+/(?P<sample>[a-zA-Z0-9]+).recal_data.csv'),
         filter=formatter('.+/(?P<readid>[a-zA-Z0-9-\.]+)_(?P<lib>[a-zA-Z0-9-]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9]+).recal_data.csv'),
-        add_inputs=add_inputs('{path[0]}/{sample[0]}.sort.dedup.realn.bam'),
-        # add_inputs=add_inputs('alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.bam'),
+        # add_inputs=add_inputs('{path[0]}/{sample[0]}.sort.dedup.realn.bam'),
+        add_inputs=add_inputs('alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.bam'),
         # output='{path[0]}/{sample[0]}.sort.dedup.realn.recal.bam')
-        output='{alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.recal.bam')
+        output='alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.recal.bam')
         .follows('local_realignment_gatk'))
+
     #
     # # Call variants using GATK
     # pipeline.transform(
