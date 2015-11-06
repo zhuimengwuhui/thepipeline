763331eea452b8b80c5441fdb26db692f71a89fb
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-11-06 13:32:17 +1100

    add picard merge bam

--- src/pipeline.py
+++ src/pipeline.py
@@ -111,7 +111,8 @@ def make_pipeline(state):
         task_func=stages.merge_sample_bams,
         name='merge_sample_bams',
         filter=formatter('.+/(?P<readid>[a-zA-Z0-9-\.]+)_(?P<lib>[a-zA-Z0-9-]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9]+).sort.dedup.realn.recal.bam'),
-        inputs=add_inputs('alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.bam'),
+        # inputs=add_inputs('alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.bam'),
+        inputs=output_from('print_reads_gatk'),
         output='alignments/{sample[0]}/{sample[0]}.merged.bam')
 
     #
