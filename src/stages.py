793c7c899d947a9eb4b286018be72875a9f5ebf9
Khalid Mahmood <mahmood.khalid@gmail.com>
2015-11-10 11:10:30 +1100

    adding extra annotation types

--- src/stages.py
+++ src/stages.py
@@ -168,12 +168,37 @@ class Stages(object):
         # safe_make_dir('variants}'.format(sample=sample_id))
         gatk_args = "-T HaplotypeCaller -R {reference} --min_base_quality_score 20 " \
                     "--standard_min_confidence_threshold_for_calling 30.0 " \
+                    "--variant_index_type LINEAR " \
+                    "--standard_min_confidence_threshold_for_emitting 30.0 " \
+                    "-A AlleleBalance -A AlleleBalanceBySample " \
+                    "-A ChromosomeCounts -A ClippingRankSumTest " \
+                    "-A Coverage -A DepthPerAlleleBySample " \
+                    "-A DepthPerSampleHC -A FisherStrand " \
+                    "-A GCContent -A GenotypeSummaries " \
+                    "-A HaplotypeScore -A HardyWeinberg " \
+                    "-A HomopolymerRun -A InbreedingCoeff " \
+                    "-A LikelihoodRankSumTest -A LowMQ " \
+                    "-A MappingQualityRankSumTest -A MappingQualityZero " \
+                    "-A PossibleDeNovo -A QualByDepth " \
+                    "-A RMSMappingQuality -A ReadPosRankSumTest " \
+                    "-A SampleList -A SpanningDeletions " \
+                    "-A StrandBiasBySample -A StrandOddsRatio " \
+                    "-A TandemRepeatAnnotator -A VariantType " \
+                    "-I {bam} -L {interval_list} -o {out}".format(reference=self.reference,
+                                                          bam=bam_in, interval_list=self.interval_hg19, out=vcf_out)
+        self.run_gatk('call_haplotypecaller_gatk', )
+
+    def call_haplotypecaller_gatk_nct(self, bam_in, vcf_out):
+        '''Call variants using GATK'''
+        # safe_make_dir('variants}'.format(sample=sample_id))
+        gatk_args = "-T HaplotypeCaller -R {reference} --min_base_quality_score 20 " \
+                    "--standard_min_confidence_threshold_for_calling 30.0 " \
                     "--num_cpu_threads_per_data_thread 8 " \
                     "--variant_index_type LINEAR " \
                     "--standard_min_confidence_threshold_for_emitting 30.0 " \
                     "-I {bam} -L {interval_list} -o {out}".format(reference=self.reference,
                                                           bam=bam_in, interval_list=self.interval_hg19, out=vcf_out)
-        self.run_gatk('call_haplotypecaller_gatk', gatk_args)
+        self.run_gatk('call_haplotypecaller_gatk', )
 
     def combine_gvcf_gatk(self, vcf_files_in, vcf_out):
         '''Combine G.VCF files for all samples using GATK'''
@@ -233,6 +258,14 @@ class Stages(object):
                     "-resource:mills,known=false,training=true,truth=true,prior=12.0 {mills_hg19} " \
                     "-resource:1000G,known=false,training=true,truth=true,prior=10.0 {one_k_g_indels} " \
                     "-an MQRankSum -an ReadPosRankSum -an FS -input {genotype_vcf} -recalFile {recal_indel} " \
+                    "-an DP " \
+                    "-an QD " \
+                    "-an FS " \
+                    "-an MQRankSum " \
+                    "-an ReadPosRankSum " \
+                    "-an MQ " \
+                    "-an SOR " \
+                    "-an InbreedingCoeff " \
                     "-tranchesFile {tranches_indel} -rscriptFile {indel_plots} " \
                     " -mode INDEL".format(reference=self.reference,
                                           cores=cores, mills_hg19=self.mills_hg19, one_k_g_indels=self.one_k_g_indels,
