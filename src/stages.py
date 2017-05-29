e290f07228af62c9ea144c57c160d834561488f3
Khalid Mahmood <mahmood.khalid@gmail.com>
2017-05-29 15:02:35 +1000

    updated filter

--- src/stages.py
+++ src/stages.py
@@ -333,6 +333,7 @@ class Stages(object):
                     "--filterExpression \"DP < 10\" --filterName \"LowCoverage\" " \
                     "--filterExpression \"MQ < 40\" --filterName \"LowMappingQual\" " \
                     "--filterExpression \"SOR > 4.0\" --filterName \"StrandBias\" " \
+                    "--filterExpression \"HRun > 12.0\" --filterName \"HRun12\" " \
                     "--variant {vcf_in} -o {vcf_out}".format(reference=self.reference,
                                                       cores=cores, vcf_in=vcf_in, vcf_out=vcf_out)
         self.run_gatk('apply_variant_filtration_gatk_lenient', gatk_args)
