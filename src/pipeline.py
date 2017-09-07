7d33ba9615c6da07b397ac3170d86cb781a17d7e
Khalid Mahmood <mahmood.khalid@gmail.com>
2017-09-07 15:32:20 +1000

    re add gridss

--- src/pipeline.py
+++ src/pipeline.py
@@ -320,13 +320,13 @@ def make_pipeline(state):
         output='delly/germline.DEL.bcf')
 
     # Call GRIDSS
-    # pipeline.transform(
-    #     task_func=stages.apply_gridss,
-    #     name='apply_gridss',
-    #     input=output_from('merge_sample_bams'),
-    #     filter=formatter('.+/(?P<sample>[a-zA-Z0-9]+).merged.bam'),
-    #     extras=['{sample[0]}'],
-    #     output='svariants/{sample[0]}/{sample[0]}.gridss.sv.vcf')
+    pipeline.transform(
+        task_func=stages.apply_gridss,
+        name='apply_gridss',
+        input=output_from('merge_sample_bams'),
+        filter=formatter('.+/(?P<sample>[a-zA-Z0-9]+).merged.bam'),
+        extras=['{sample[0]}'],
+        output='svariants/{sample[0]}/{sample[0]}.gridss.sv.vcf')
 
     # # Call DUPs with DELLY
     # pipeline.merge(
