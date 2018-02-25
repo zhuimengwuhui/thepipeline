2c17973daddb2facb40ca9d2a5b620ec95058f9d
jianan <jianan@jiananwangdeMacBook-Air.local>
2018-02-25 20:19:53 +1100

    modify pipeline and stages and also config

--- src/pipeline.py
+++ src/pipeline.py
@@ -75,4 +75,169 @@ def make_pipeline(state):
         filter=suffix('.sort.dedup.bam'),
         output='.intervals')
 
+      # Local realignment using GATK
+    (pipeline.transform(
+        task_func=stages.local_realignment_gatk,
+        name='local_realignment_gatk',
+        input=output_from('realigner_target_creator'),
+        # filter=formatter('.+/(?P<sample>[a-zA-Z0-9]+).chr.intervals'),
+        filter=formatter(
+            '.+/(?P<readid>[a-zA-Z0-9-]+)_(?P<lib>[a-zA-Z0-9-:]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9-]+).intervals'),
+        # add_inputs=add_inputs('{path[0]}/{sample[0]}.sort.dedup.bam'),
+        add_inputs=add_inputs(
+            'alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.bam'),
+        output='alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.bam')
+        .follows('mark_duplicates_picard'))
+
+    # Base recalibration using GATK
+    pipeline.transform(
+        task_func=stages.base_recalibration_gatk,
+        name='base_recalibration_gatk',
+        input=output_from('local_realignment_gatk'),
+        filter=suffix('.sort.dedup.realn.bam'),
+        output=['.recal_data.csv', '.count_cov.log'])
+
+    # Print reads using GATK
+    (pipeline.transform(
+        task_func=stages.print_reads_gatk,
+        name='print_reads_gatk',
+        input=output_from('base_recalibration_gatk'),
+        # filter=formatter('.+/(?P<sample>[a-zA-Z0-9]+).recal_data.csv'),
+        filter=formatter(
+            # '.+/(?P<readid>[a-zA-Z0-9-\.]+)_(?P<lib>[a-zA-Z0-9-]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9]+).recal_data.csv'),
+            '.+/(?P<readid>[a-zA-Z0-9-]+)_(?P<lib>[a-zA-Z0-9-:]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9-]+).recal_data.csv'),
+            # '.+/(?P<readid>[a-zA-Z0-9-]+)_(?P<lib>[a-zA-Z0-9-:]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9-]+).recal_data.csv'),
+        # add_inputs=add_inputs('{path[0]}/{sample[0]}.sort.dedup.realn.bam'),
+        add_inputs=add_inputs(
+            'alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.bam'),
+        # output='{path[0]}/{sample[0]}.sort.dedup.realn.recal.bam')
+        output='alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.recal.bam')
+        .follows('local_realignment_gatk'))
+
+    # Merge lane bams to sample bams
+    pipeline.collate(
+        task_func=stages.merge_sample_bams,
+        name='merge_sample_bams',
+        filter=formatter(
+            # '.+/(?P<readid>[a-zA-Z0-9-\.]+)_(?P<lib>[a-zA-Z0-9-]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9]+).sort.dedup.realn.recal.bam'),
+            '.+/(?P<readid>[a-zA-Z0-9-]+)_(?P<lib>[a-zA-Z0-9-:]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9-]+).sort.dedup.realn.recal.bam'),
+        # inputs=add_inputs('alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.bam'),
+        input=output_from('print_reads_gatk'),
+        output='alignments/{sample[0]}/{sample[0]}.merged.bam')
+
+    # Mark duplicates in the BAM file using Picard
+    pipeline.transform(
+        task_func=stages.mark_duplicates_picard,
+        name='mark_duplicates_picard2',
+        input=output_from('merge_sample_bams'),
+        # filter=formatter(
+        # '.+/(?P<readid>[a-zA-Z0-9-\.]+)_(?P<lib>[a-zA-Z0-9-]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9]+).merged.bam'),
+        filter=suffix('.merged.bam'),
+        # XXX should make metricsup an extra output?
+        output=['.merged.dedup.bam', '.metricsdup'])
+
+    # Local realignment2 using GATK
+    # Generate RealignerTargetCreator using GATK
+    pipeline.transform(
+        task_func=stages.realigner_target_creator,
+        name='realigner_target_creator2',
+        input=output_from('mark_duplicates_picard2'),
+        filter=suffix('.dedup.bam'),
+        output='.intervals')
+
+    # Local realignment using GATK
+    (pipeline.transform(
+        task_func=stages.local_realignment_gatk,
+        name='local_realignment_gatk2',
+        input=output_from('realigner_target_creator2'),
+        filter=formatter('.+/(?P<sample>[a-zA-Z0-9-]+).merged.intervals'),
+        # filter=formatter(
+        # '.+/(?P<readid>[a-zA-Z0-9-\.]+)_(?P<lib>[a-zA-Z0-9-]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9]+).intervals'),
+        # add_inputs=add_inputs('{path[0]}/{sample[0]}.sort.dedup.bam'),
+        add_inputs=add_inputs(
+            'alignments/{sample[0]}/{sample[0]}.merged.dedup.bam'),
+        output='alignments/{sample[0]}/{sample[0]}.merged.dedup.realn.bam')
+        .follows('mark_duplicates_picard2'))
+
+    # Call variants using GATK
+    pipeline.transform(
+        task_func=stages.call_haplotypecaller_gatk,
+        name='call_haplotypecaller_gatk',
+        input=output_from('local_realignment_gatk2'),
+        # filter=suffix('.merged.dedup.realn.bam'),
+        filter=formatter('.+/(?P<sample>[a-zA-Z0-9-]+).merged.dedup.realn.bam'),
+        output='variants/{sample[0]}.g.vcf')
+
+    # Combine G.VCF files for all samples using GATK
+    pipeline.merge(
+        task_func=stages.combine_gvcf_gatk,
+        name='combine_gvcf_gatk',
+        input=output_from('call_haplotypecaller_gatk'),
+        output='variants/ALL.combined.vcf')
+
+    # Genotype G.VCF files using GATK
+    pipeline.transform(
+        task_func=stages.genotype_gvcf_gatk,
+        name='genotype_gvcf_gatk',
+        input=output_from('combine_gvcf_gatk'),
+        filter=suffix('.combined.vcf'),
+        output='.raw.vcf')
+
+    # SNP recalibration using GATK
+    pipeline.transform(
+        task_func=stages.snp_recalibrate_gatk,
+        name='snp_recalibrate_gatk',
+        input=output_from('genotype_gvcf_gatk'),
+        filter=suffix('.raw.vcf'),
+        output=['.snp_recal', '.snp_tranches', '.snp_plots.R'])
+
+    # INDEL recalibration using GATK
+    pipeline.transform(
+        task_func=stages.indel_recalibrate_gatk,
+        name='indel_recalibrate_gatk',
+        input=output_from('genotype_gvcf_gatk'),
+        filter=suffix('.raw.vcf'),
+        output=['.indel_recal', '.indel_tranches', '.indel_plots.R'])
+
+    # Apply SNP recalibration using GATK
+    (pipeline.transform(
+        task_func=stages.apply_snp_recalibrate_gatk,
+        name='apply_snp_recalibrate_gatk',
+        input=output_from('genotype_gvcf_gatk'),
+        filter=suffix('.raw.vcf'),
+        add_inputs=add_inputs(['ALL.snp_recal', 'ALL.snp_tranches']),
+        output='.recal_SNP.vcf')
+        .follows('snp_recalibrate_gatk'))
+
+    # Apply INDEL recalibration using GATK
+    (pipeline.transform(
+        task_func=stages.apply_indel_recalibrate_gatk,
+        name='apply_indel_recalibrate_gatk',
+        input=output_from('genotype_gvcf_gatk'),
+        filter=suffix('.raw.vcf'),
+        add_inputs=add_inputs(
+            ['ALL.indel_recal', 'ALL.indel_tranches']),
+        output='.recal_INDEL.vcf')
+        .follows('indel_recalibrate_gatk'))
+
+    # Combine variants using GATK
+    (pipeline.transform(
+        task_func=stages.combine_variants_gatk,
+        name='combine_variants_gatk',
+        input=output_from('apply_snp_recalibrate_gatk'),
+        filter=suffix('.recal_SNP.vcf'),
+        add_inputs=add_inputs(['ALL.recal_INDEL.vcf']),
+        # output='.combined.vcf')
+        output='ALL.raw.vqsr.vcf')
+        .follows('apply_indel_recalibrate_gatk'))
+    #
+    # # Select variants using GATK
+    # pipeline.transform(
+    #     task_func=stages.select_variants_gatk,
+    #     name='select_variants_gatk',
+    #     input=output_from('combine_variants_gatk'),
+    #     filter=suffix('.combined.vcf'),
+    #     output='.selected.vcf')
+
+
     return pipeline
--- src/stages.py
+++ src/stages.py
@@ -35,6 +35,7 @@ class Stages(object):
         self.reference = self.get_options('reference')
         self.known_dog_snps = self.get_options('known_dog_snps')
         self.known_dog_indels = self.get_options('known_dog_indels')
+        self.interval_dog = self.get_options('interval_bed_dog')
 
     def run_picard(self, stage, args):
         mem = int(self.state.config.get_stage_options(stage, 'mem'))
@@ -100,7 +101,196 @@ class Stages(object):
                     '--known {one_k_g_indels} ' \
                     '--known {one_k_g_indels} ' \
                     '-o {out}'.format(reference=self.reference, bam=bam_in,
-                                      threads=cores, 
+                                      threads=cores,
                                       one_k_g_indels=self.known_dog_indels,
                                       out=intervals_out)
         self.run_gatk('chrom_intervals_gatk', gatk_args)
+
+    def local_realignment_gatk(self, inputs, bam_out):
+        '''Local realign reads using GATK'''
+        target_intervals_in, bam_in = inputs
+        gatk_args = "-T IndelRealigner -R {reference} -I {bam} " \
+                    "-targetIntervals {target_intervals} " \
+                    "-known {one_k_g_indels} " \
+                    "-o {out}".format(reference=self.reference, bam=bam_in,
+                                      one_k_g_indels=self.known_dog_indels,
+                                      target_intervals=target_intervals_in,
+                                      out=bam_out)
+        self.run_gatk('local_realignment_gatk', gatk_args)
+
+    # XXX I'm not sure that --num_cpu_threads_per_data_thread has any benefit
+    # here
+    def base_recalibration_gatk(self, bam_in, outputs):
+        '''Base recalibration using GATK'''
+        csv_out, log_out = outputs
+        gatk_args = "-T BaseRecalibrator -R {reference} -I {bam} " \
+                    "--num_cpu_threads_per_data_thread 4 --knownSites {dbsnp_hg19} " \
+                    "--knownSites {one_k_g_indels} " \
+                    "-log {log} -o {out}".format(reference=self.reference, bam=bam_in,
+                                                 dbsnp_hg19=self.known_dog_snps,
+                                                 one_k_g_indels=self.known_dog_indels,
+                                                 log=log_out, out=csv_out)
+        self.run_gatk('base_recalibration_gatk', gatk_args)
+
+    # XXX I'm not sure that --num_cpu_threads_per_data_thread has any benefit
+    # here
+    def print_reads_gatk(self, inputs, bam_out):
+        '''Print reads using GATK'''
+        [csv_in, _log], bam_in = inputs
+        gatk_args = "-T PrintReads -R {reference} -I {bam} --BQSR {recal_csv} " \
+                    "-o {out} --num_cpu_threads_per_data_thread 4".format(reference=self.reference,
+                                                                          bam=bam_in, recal_csv=csv_in, out=bam_out)
+        self.run_gatk('print_reads_gatk', gatk_args)
+
+    # Merge per lane bam into a single bam per sample
+    def merge_sample_bams(self, bam_files_in, bam_out):
+        '''Merge per lane bam into a merged bam file'''
+        bam_files = ' '.join(['INPUT=' + bam for bam in bam_files_in])
+        picard_args = 'MergeSamFiles {bams_in} OUTPUT={merged_bam_out} ' \
+                      'VALIDATION_STRINGENCY=LENIENT ' \
+                      'MAX_RECORDS_IN_RAM=5000000 ASSUME_SORTED=True ' \
+                      'CREATE_INDEX=True'.format(
+                          bams_in=bam_files, merged_bam_out=bam_out)
+        self.run_picard('merge_sample_bams', picard_args)
+
+    def call_haplotypecaller_gatk(self, bam_in, vcf_out):
+        '''Call variants using GATK'''
+        # safe_make_dir('variants}'.format(sample=sample_id))
+        gatk_args = "-T HaplotypeCaller -R {reference} --min_base_quality_score 20 " \
+                    "--emitRefConfidence GVCF " \
+                    "-A AlleleBalance -A AlleleBalanceBySample " \
+                    "-A ChromosomeCounts -A ClippingRankSumTest " \
+                    "-A Coverage -A DepthPerAlleleBySample " \
+                    "-A DepthPerSampleHC -A FisherStrand " \
+                    "-A GCContent -A GenotypeSummaries " \
+                    "-A HardyWeinberg -A HomopolymerRun " \
+                    "-A LikelihoodRankSumTest -A LowMQ " \
+                    "-A MappingQualityRankSumTest -A MappingQualityZero " \
+                    "-A QualByDepth " \
+                    "-A RMSMappingQuality -A ReadPosRankSumTest " \
+                    "-A SampleList -A SpanningDeletions " \
+                    "-A StrandBiasBySample -A StrandOddsRatio " \
+                    "-A TandemRepeatAnnotator -A VariantType " \
+                    "-I {bam} -L {interval_list} -o {out}".format(reference=self.reference,
+                                                                  bam=bam_in, interval_list=self.interval_dog, out=vcf_out)
+        self.run_gatk('call_haplotypecaller_gatk', gatk_args)
+
+    def call_haplotypecaller_gatk_nct(self, bam_in, vcf_out):
+        '''Call variants using GATK'''
+        # safe_make_dir('variants}'.format(sample=sample_id))
+        gatk_args = "-T HaplotypeCaller -R {reference} --min_base_quality_score 20 " \
+                    "--standard_min_confidence_threshold_for_calling 30.0 " \
+                    "--num_cpu_threads_per_data_thread 4 " \
+                    "--variant_index_type LINEAR " \
+                    "--standard_min_confidence_threshold_for_emitting 30.0 " \
+                    "-I {bam} -L {interval_list} -o {out}".format(reference=self.reference,
+                                                                  bam=bam_in, interval_list=self.interval_dog, out=vcf_out)
+        self.run_gatk('call_haplotypecaller_gatk', gatk_args)
+
+    def combine_gvcf_gatk(self, vcf_files_in, vcf_out):
+        '''Combine G.VCF files for all samples using GATK'''
+        g_vcf_files = ' '.join(['--variant ' + vcf for vcf in vcf_files_in])
+        gatk_args = "-T CombineGVCFs -R {reference} " \
+                    "--disable_auto_index_creation_and_locking_when_reading_rods " \
+                    "{g_vcf_files} -o {vcf_out}".format(reference=self.reference,
+                                                        g_vcf_files=g_vcf_files, vcf_out=vcf_out)
+        # "{g_vcf_files} -o {vcf_out} --variant {CEU}".format(reference=self.reference,
+        # g_vcf_files=g_vcf_files, vcf_out=vcf_out, CEU=self.CEU_mergeGvcf)
+        self.run_gatk('combine_gvcf_gatk', gatk_args)
+
+    def genotype_gvcf_gatk(self, merged_vcf_in, vcf_out):
+        '''Genotype G.VCF files using GATK'''
+        cores = self.get_stage_options('genotype_gvcf_gatk', 'cores')
+        gatk_args = "-T GenotypeGVCFs -R {reference} " \
+                    "--disable_auto_index_creation_and_locking_when_reading_rods " \
+                    "-A AlleleBalance -A AlleleBalanceBySample " \
+                    "-A ChromosomeCounts -A ClippingRankSumTest " \
+                    "-A Coverage -A DepthPerAlleleBySample " \
+                    "-A DepthPerSampleHC -A FisherStrand " \
+                    "-A GCContent -A GenotypeSummaries " \
+                    "-A HardyWeinberg -A HomopolymerRun " \
+                    "-A LikelihoodRankSumTest " \
+                    "-A MappingQualityRankSumTest -A MappingQualityZero " \
+                    "-A QualByDepth " \
+                    "-A RMSMappingQuality -A ReadPosRankSumTest " \
+                    "-A SampleList -A SpanningDeletions " \
+                    "-A StrandBiasBySample -A StrandOddsRatio " \
+                    "-A TandemRepeatAnnotator -A VariantType " \
+                    "--dbsnp {dbsnp} " \
+                    "--num_threads {cores} --variant {merged_vcf} --out {vcf_out}" \
+                    .format(reference=self.reference, dbsnp=self.known_dog_snps,
+                            cores=cores, merged_vcf=merged_vcf_in, vcf_out=vcf_out)
+        self.run_gatk('genotype_gvcf_gatk', gatk_args)
+
+     def snp_recalibrate_gatk(self, genotype_vcf_in, outputs):
+        '''SNP recalibration using GATK'''
+        recal_snp_out, tranches_snp_out, snp_plots_r_out = outputs
+        cores = self.get_stage_options('snp_recalibrate_gatk', 'cores')
+        gatk_args = "-T VariantRecalibrator --disable_auto_index_creation_and_locking_when_reading_rods " \
+                    "-R {reference} --minNumBadVariants 5000 --num_threads {cores} " \
+                    "-resource:omni,known=false,training=true,truth=true,prior=12.0 {one_k_g_snps} " \
+                    "-an DP -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR " \
+                    "-input {genotype_vcf} --recal_file {recal_snp} --tranches_file {tranches_snp} " \
+                    "-rscriptFile {snp_plots} -mode SNP".format(reference=self.reference,
+                                                                cores=cores, one_k_g_snps=self.known_dog_snps,
+                                                                genotype_vcf=genotype_vcf_in,
+                                                                recal_snp=recal_snp_out, tranches_snp=tranches_snp_out, snp_plots=snp_plots_r_out)
+        self.run_gatk('snp_recalibrate_gatk', gatk_args)
+
+    def indel_recalibrate_gatk(self, genotype_vcf_in, outputs):
+        '''INDEL recalibration using GATK'''
+        recal_indel_out, tranches_indel_out, indel_plots_r_out = outputs
+        cores = self.get_stage_options('indel_recalibrate_gatk', 'cores')
+        gatk_args = "-T VariantRecalibrator --disable_auto_index_creation_and_locking_when_reading_rods " \
+                    "-R {reference} --minNumBadVariants 5000 --num_threads {cores} " \
+                    "-resource:1000G,known=false,training=true,truth=true,prior=10.0 {one_k_g_indels} " \
+                    "-an DP -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR " \
+                    "-input {genotype_vcf} -recalFile {recal_indel} " \
+                    "-tranchesFile {tranches_indel} -rscriptFile {indel_plots} " \
+                    " -mode INDEL --maxGaussians 4".format(reference=self.reference,
+                                          cores=cores, one_k_g_indels=self.known_dog_indels,
+                                          genotype_vcf=genotype_vcf_in, recal_indel=recal_indel_out,
+                                          tranches_indel=tranches_indel_out, indel_plots=indel_plots_r_out)
+        self.run_gatk('indel_recalibrate_gatk', gatk_args)
+
+    def apply_snp_recalibrate_gatk(self, inputs, vcf_out):
+        '''Apply SNP recalibration using GATK'''
+        genotype_vcf_in, [recal_snp, tranches_snp] = inputs
+        cores = self.get_stage_options('apply_snp_recalibrate_gatk', 'cores')
+        gatk_args = "-T ApplyRecalibration --disable_auto_index_creation_and_locking_when_reading_rods " \
+                    "-R {reference} --ts_filter_level 99.5 --excludeFiltered --num_threads {cores} " \
+                    "-input {genotype_vcf} -recalFile {recal_snp} -tranchesFile {tranches_snp} " \
+                    "-mode SNP -o {vcf_out}".format(reference=self.reference,
+                                                    cores=cores, genotype_vcf=genotype_vcf_in, recal_snp=recal_snp,
+                                                    tranches_snp=tranches_snp, vcf_out=vcf_out)
+        self.run_gatk('apply_snp_recalibrate_gatk', gatk_args)
+
+    def apply_indel_recalibrate_gatk(self, inputs, vcf_out):
+        '''Apply INDEL recalibration using GATK'''
+        genotype_vcf_in, [recal_indel, tranches_indel] = inputs
+        cores = self.get_stage_options('apply_indel_recalibrate_gatk', 'cores')
+        gatk_args = "-T ApplyRecalibration --disable_auto_index_creation_and_locking_when_reading_rods " \
+                    "-R {reference} --ts_filter_level 99.0 --excludeFiltered --num_threads {cores} " \
+                    "-input {genotype_vcf} -recalFile {recal_indel} -tranchesFile {tranches_indel} " \
+                    "-mode INDEL -o {vcf_out}".format(reference=self.reference,
+                                                      cores=cores, genotype_vcf=genotype_vcf_in, recal_indel=recal_indel,
+                                                      tranches_indel=tranches_indel, vcf_out=vcf_out)
+        self.run_gatk('apply_indel_recalibrate_gatk', gatk_args)
+
+    def combine_variants_gatk(self, inputs, vcf_out):
+        '''Combine variants using GATK'''
+        recal_snp, [recal_indel] = inputs
+        cores = self.get_stage_options('combine_variants_gatk', 'cores')
+        gatk_args = "-T CombineVariants -R {reference} --disable_auto_index_creation_and_locking_when_reading_rods " \
+                    "--num_threads {cores} --genotypemergeoption UNSORTED --variant {recal_snp} " \
+                    "--variant {recal_indel} -o {vcf_out}".format(reference=self.reference,
+                                                                  cores=cores, recal_snp=recal_snp, recal_indel=recal_indel,
+                                                                  vcf_out=vcf_out)
+        self.run_gatk('combine_variants_gatk', gatk_args)
+
+    def select_variants_gatk(self, combined_vcf, vcf_out):
+        '''Select variants using GATK'''
+        gatk_args = "-T SelectVariants -R {reference} --disable_auto_index_creation_and_locking_when_reading_rods " \
+                    "--variant {combined_vcf} -select 'DP > 100' -o {vcf_out}".format(reference=self.reference,
+                                                                                      combined_vcf=combined_vcf, vcf_out=vcf_out)
+        self.run_gatk('select_variants_gatk', gatk_args)
