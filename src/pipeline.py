9e507b6a12723fd0f4c86e2d6c666df70f9bbda0
jianan <jianan@jiananwangdeMacBook-Air.local>
2018-02-27 14:41:18 +1100

    alter input file name regex

--- src/pipeline.py
+++ src/pipeline.py
@@ -105,7 +105,7 @@ def make_pipeline(state):
         # filter=formatter('.+/(?P<sample>[a-zA-Z0-9]+).recal_data.csv'),
         filter=formatter(
             # '.+/(?P<readid>[a-zA-Z0-9-\.]+)_(?P<lib>[a-zA-Z0-9-]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9]+).recal_data.csv'),
-            '.+/(?P<sample>[a-zA-Z0-9-]+)_(?P<readid>[a-zA-Z0-9-:]+)_(?P<lib>[a-zA-Z0-9]+)_(?P<lane>[a-zA-Z0-9-]+).recal_data.csv'),
+            '.+/(?P<readid>[a-zA-Z0-9-]+)_(?P<lib>[a-zA-Z0-9-:]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9-]+).recal_data.csv'),
             # '.+/(?P<readid>[a-zA-Z0-9-]+)_(?P<lib>[a-zA-Z0-9-:]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9-]+).recal_data.csv'),
         # add_inputs=add_inputs('{path[0]}/{sample[0]}.sort.dedup.realn.bam'),
         add_inputs=add_inputs(
@@ -120,7 +120,7 @@ def make_pipeline(state):
         name='merge_sample_bams',
         filter=formatter(
             # '.+/(?P<readid>[a-zA-Z0-9-\.]+)_(?P<lib>[a-zA-Z0-9-]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9]+).sort.dedup.realn.recal.bam'),
-            '.+/(?P<sample>[a-zA-Z0-9-]+)_(?P<readid>[a-zA-Z0-9-:]+)_(?P<lib>[a-zA-Z0-9]+)_(?P<lane>[a-zA-Z0-9-]+).sort.dedup.realn.recal.bam'),
+            '.+/(?P<readid>[a-zA-Z0-9-]+)_(?P<lib>[a-zA-Z0-9-:]+)_(?P<lane>[a-zA-Z0-9]+)_(?P<sample>[a-zA-Z0-9-]+).sort.dedup.realn.recal.bam'),
         # inputs=add_inputs('alignments/{sample[0]}/{readid[0]}_{lib[0]}_{lane[0]}_{sample[0]}.sort.dedup.realn.bam'),
         input=output_from('print_reads_gatk'),
         output='alignments/{sample[0]}/{sample[0]}.merged.bam')
