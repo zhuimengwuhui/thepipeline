c49315f13e599e789f7aa5dc5caf3f5782eeb39b
jianan <jianan@jiananwangdeMacBook-Air.local>
2018-02-27 14:05:50 +1100

    modify name

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@
 from setuptools import setup
 
 setup(
-    name='thepipeline',
+    name='dog_pipeline',
     version='0.1',
     author='Khalid Mahmood',
     author_email='khalid.mahmood@unimelb.edu.au',
     packages=['src'],
     entry_points={
-        'console_scripts': ['thepipeline = src.main:main']
+        'console_scripts': ['dog_pipeline = src.main:main']
     },
     url='https://github.com/khalidm/thepipepine',
     license='LICENSE.txt',
-    description='thepipeline is a bioinformatics pipeline to filter and \
-     annotate a vcf file.',
+    description='dog_pipeline is a bioinformatics pipeline to call variants in dog exome data.',
     long_description=open('README.md').read(),
     install_requires=[
         "ruffus == 2.6.3",
